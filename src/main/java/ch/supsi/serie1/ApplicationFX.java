1107ca53a710a7e4057bcd5243d96b9581363d53
Davide Ravani <davide.ravani@student.supsi.ch>
2021-03-16 08:38:29 +0100

    Added logging panel

--- src/main/java/ch/supsi/serie1/ApplicationFX.java
+++ src/main/java/ch/supsi/serie1/ApplicationFX.java
@@ -3,11 +3,15 @@ package ch.supsi.serie1;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Background;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javafx.application.Application;
@@ -41,7 +45,7 @@ public class ApplicationFX extends Application{
 
         ListView<Student> studentListView = new ListView<>();
 
-        Pane root = new Pane(courseListView,studentListView);
+        Pane coursePane = new Pane(courseListView,studentListView);
 
         studentListView.setLayoutX(200);
         studentListView.setMaxSize(200,300);
@@ -49,6 +53,15 @@ public class ApplicationFX extends Application{
 
         courseListView.setStyle("-fx-selection-bar: blue;");
         studentListView.setStyle("-fx-background-color: green;");
+
+        TextArea logText = new TextArea();
+        Pane logPanel = new Pane(logText);
+
+        logText.setMaxSize(400, 200);
+
+        BorderPane root = new BorderPane();
+        root.setCenter(coursePane);
+        root.setBottom(logPanel);
 
         stage.setScene(new Scene(root,400,300));
         stage.show();
@@ -60,8 +73,11 @@ public class ApplicationFX extends Application{
 
             studentListView.getItems().addAll(t1.getStudents());
 
-        });
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
 
+            logText.appendText(formatter.format(date) + " " +t1.toString() + "\n");
+        });
     }
 
     public static void main(String[] args) {
