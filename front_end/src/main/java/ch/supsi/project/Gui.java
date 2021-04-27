b8d6483c54af54c1655333f3f9218d591679c0e8
lorenzo <lorenzo.ronzani@student.supsi.ch>
2021-04-27 09:02:55 +0200

    Base

--- front_end/src/main/java/ch/supsi/project/Gui.java
+++ front_end/src/main/java/ch/supsi/project/Gui.java
@@ -1,11 +1,19 @@
 package ch.supsi.project;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Gui extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Calendario");
+
+        BorderPane root = new BorderPane();
+
+        stage.setScene(new Scene(root,900,700));
+        stage.show();
 
     }
 
