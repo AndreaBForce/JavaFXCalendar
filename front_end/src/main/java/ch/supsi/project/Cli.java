5845b14a8c0818747d4f214b10ff498f76e4af41
lorenzo <lorenzo.ronzani@student.supsi.ch>
2021-05-04 10:22:30 +0200

    salvataggio su file

--- front_end/src/main/java/ch/supsi/project/Cli.java
+++ front_end/src/main/java/ch/supsi/project/Cli.java
@@ -1,7 +1,7 @@
 package ch.supsi.project;
 
 import ch.supsi.project.application_layer.Colour;
-import ch.supsi.project.application_layer.Type;
+import ch.supsi.project.service_layer.Type;
 import ch.supsi.project.service_layer.*;
 
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Cli {
     public static void main(String[] args) {
-        CalendarContainer calendarContainer = new CalendarContainer();
+        CalendarContainer calendarContainer = new CalendarContainer("Prova.txt");
         List<EventType> eventTypeList = new ArrayList<>();
 
         eventTypeList.add(new EventType(Type.LECTION, Colour.BLUE));
@@ -28,14 +28,16 @@ public class Cli {
         cal.add(Calendar.HOUR_OF_DAY, 3);
         long end = cal.getTime().getTime();
 
-        calendarContainer.addEvent(new Event("1",time.getTime(),start,end, eventTypeList.get(0)));
+        /*calendarContainer.addEvent(new Event("1",time.getTime(),start,end, eventTypeList.get(0)));
         calendarContainer.addEvent(new Event("2",time.getTime(),start,end, eventTypeList.get(1)));
         calendarContainer.addEvent(new Event("3",time.getTime(),start,end, eventTypeList.get(2)));
         calendarContainer.addEvent(new Event("4",time.getTime(),start,end, eventTypeList.get(3)));
-        calendarContainer.addEvent(new Event("5",time.getTime(),start,end, eventTypeList.get(4)));
+        calendarContainer.addEvent(new Event("5",time.getTime(),start,end, eventTypeList.get(4)));*/
 
         List<Event> list = calendarContainer.getCalendar();
 
         list.forEach(System.out::println);
+
+        calendarContainer.close();
     }
 }
